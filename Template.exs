a81f27b5b35af367c4dcb5e7df1c38f4e65a8689
Nathan Paul Simons <nathan@hardcorehackers.com>
2020-05-12 22:24:19 -0700

    Added template for Elixir.

---
+++ Template.exs
@@ -0,0 +1,22 @@
+#! /usr/bin/env elixir
+# Copyright (C) 2020 Nathan Paul Simons (C2T9uE-code@hardcorehackers.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+IO.puts("Hello world!")
+
+# Local Variables:
+#   mode: Elixir
+# End:
+# vi: fileformat=unix expandtab
